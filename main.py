0ce72b49dbf48e6cea7e24722cc8d1da9109f789
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2022-07-28 21:28:24 +0200

    For testing

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ c = CommunicateToSmartContract()
 
 
 def main():
+    st = time.time()
     d.refreshToken()
+    tt = time.time()
+    print('Execution time (refreshing token):', tt - st, 'seconds')
     d.saveAsJson(d.getAllWorkingItems(d.getAllWorkingItemsQuery), "AllWorkingItems")
     firstTimeRun()
+    et = time.time()
+    print('Execution time (first time run):', et - st, 'seconds')
     while True:
         d.refreshToken()
         refetchingFreezingAndUpdating()
@@ -28,7 +33,8 @@ def refetchingFreezingAndUpdating():
     if updated_ids:
         print("to update: ", updated_ids)
         for id in updated_ids:
-            c.uptateFormOnSmartContract(id, d.nameNewestBackupFile, [item for item in local_names if item.startswith(id)][0])
+            c.uptateFormOnSmartContract(id, d.nameNewestBackupFile,
+                                        [item for item in local_names if item.startswith(id)][0])
     else:
         print("nothing to update")
     if ids_to_freeze:
@@ -39,11 +45,12 @@ def refetchingFreezingAndUpdating():
 def creteNewFormOnSC(newFiles, local_names):
     if newFiles:
         for id, new_file in newFiles.items():
-            c.createNewFormSmartContract(id, new_file,[item for item in local_names if item.startswith(id)][0])
+            c.createNewFormSmartContract(id, new_file, [item for item in local_names if item.startswith(id)][0])
 
 
 def firstTimeRun():
     back_up_file_name = 'BackUp' + str(time.time()) + ".json"
+    stt = time.time()
     with open(r'AllWorkingItems.json', encoding='utf-8') as f:
         loaded = json.load(f)
         # save the relevant data inside a nested json called BackUp
@@ -51,10 +58,15 @@ def firstTimeRun():
         with open(back_up_file_name, 'w', encoding='utf-8') as jsonFile:
             final, local_names = d.helperFunctionForExtractionAndSaving(loaded)
             json.dump(final, jsonFile, indent=2)
+            ett = time.time()
+            print('Execution time (extracting and fetching each form separately):', ett - stt, 'seconds')
             print("ran for the fist time")
             # send the created BackUp JSON to the local BC
+            st = time.time()
             for id, val in final.items():
                 c.createNewFormSmartContract(id, val, [item for item in local_names if item.startswith(id)][0])
+            et = time.time()
+            print('Execution time (creating SC on BC):', et - st, 'seconds')
 
 
 if __name__ == '__main__':
